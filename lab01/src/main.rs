eca5f596edac39616bd3eb16f98148bc33b3216c
Mareantz <marianciotir@gmail.com>
2023-10-05 22:43:53 +0300

    small fix lab01

--- lab01/src/main.rs
+++ lab01/src/main.rs
@@ -1,94 +1,77 @@
 fn main() {
-    p3();
+    p2();
 }
 
-fn prim(nr:u32)
-{
-    if nr==1||nr==0
-    {
+fn prim(nr: u32) {
+    if nr == 1 || nr == 0 {
         println!("Numarul {nr} nu este prim");
         return;
     }
-    if nr%2==0&&nr!=2
-    {
+    if nr % 2 == 0 && nr != 2 {
         println!("Numarul {nr} nu este prim");
         return;
     }
-    let mut i:u32=3;
-    while i*i<=nr
-    {
-        if nr%i==0
-        {
+    let mut i: u32 = 3;
+    while i * i <= nr {
+        if nr % i == 0 {
             println!("Numarul {nr} nu este prim");
             return;
         }
-        i+=2;
+        i += 2;
     }
     println!("Numarul {nr} este prim");
     return;
 }
 
-fn p1()
-{
-    let mut i=0;
-    while i<=100
-    {
+fn p1() {
+    let mut i = 0;
+    while i <= 100 {
         prim(i);
-        i+=1;
+        i += 1;
     }
 }
 
-fn coprime(mut a:u32,mut b:u32)
-{
-    let mut c=a%b;
-    let nr1=a;
-    let nr2=b;
-    while c!=0
-    {
-        a=b;
-        b=c;
-        c=a%b;
+fn coprime(mut a: u32, mut b: u32) {
+    let mut c = a % b;
+    let nr1 = a;
+    let nr2 = b;
+    while a % b != 0 {
+        c = a % b;
+        a = b;
+        b = c;
     }
-    if c==1
-    {
+    if c == 1 {
         println!("Numerele {nr1} si {nr2} sunt coprime");
         return;
-    }
-    else 
-    {
+    } else {
         println!("Numerele {nr1} si {nr2} nu sunt coprime");
         return;
     }
 }
 
-fn p2()
-{
-    let mut i=0;
+fn p2() {
+    let mut i = 0;
     let mut j;
-    while i<=100
-    {
-        j=i+1;
-        while j<=100
-        {
-            coprime(i,j);
-            j+=1;
+    while i <= 100 {
+        j = i + 1;
+        while j <= 100 {
+            coprime(i, j);
+            j += 1;
         }
-        i+=1;
+        i += 1;
     }
     return;
 }
 
-fn p3()
-{
-    let mut beer=99;
-    while beer>1
-    {
+fn p3() {
+    let mut beer = 99;
+    while beer > 1 {
         println!("{beer} bottles of beer on the wall,");
         println!("{beer} bottles of beer.");
         println!("Take one down, pass it around,");
-        println!("{} bottles of beer on the wall.",beer-1);
+        println!("{} bottles of beer on the wall.", beer - 1);
         println!("");
-        beer-=1;
+        beer -= 1;
     }
     println!("{beer} bottles of beer on the wall,");
     println!("{beer} bottles of beer.");
