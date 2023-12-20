ba0fe918e8587c43fa0901814d7334df78e961ad
Marian Ciotir <marianciotir@gmail.com>
2023-12-21 00:45:45 +0200

    project init & quote command

---
+++ project/README.md
@@ -0,0 +1,1 @@
+# Serenity k9 Bot with Shuttle
---
+++ project/src/main.rs
@@ -0,0 +1,62 @@
+use anyhow::anyhow;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use tracing::{error, info};
+use std::fs;
+use rand::seq::IteratorRandom;
+
+struct Bot;
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!hello" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+                error!("Error sending message: {:?}", e);
+            }
+        }
+        if msg.content =="!quote"
+        {
+            let rng = fs::read_to_string("src/quotes.txt");
+            match rng {
+                Ok(rng) => {
+                    let quote = rng.lines().choose(&mut rand::thread_rng()).unwrap();
+                    if let Err(e) = msg.channel_id.say(&ctx.http, quote).await {
+                        error!("Error sending message: {:?}", e);
+                    }
+                }
+            Err(e) => error!("Error reading file: {:?}", e),
+            }
+            
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
+}
